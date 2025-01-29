1f591b6ac52031e0fbb4175bb8193935425d4056
David-6 <jiseop.63@gmail.com>
2025-01-30 01:24:28 +0900

    Add Character BP and ABP

--- Source/MysticSkirmisher/Private/Character/SkirmisherCharacterBase.cpp
+++ Source/MysticSkirmisher/Private/Character/SkirmisherCharacterBase.cpp
@@ -5,8 +5,11 @@
 
 ASkirmisherCharacterBase::ASkirmisherCharacterBase()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ASkirmisherCharacterBase::BeginPlay()
