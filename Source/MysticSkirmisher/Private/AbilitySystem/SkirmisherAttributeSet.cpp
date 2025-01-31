706de0a93476b14c2eb0713acdc96d8177e1cc7d
David-6 <jiseop.63@gmail.com>
2025-01-31 23:16:11 +0900

    Add vital attributes and setup accessors

--- Source/MysticSkirmisher/Private/AbilitySystem/SkirmisherAttributeSet.cpp
+++ Source/MysticSkirmisher/Private/AbilitySystem/SkirmisherAttributeSet.cpp
@@ -2,4 +2,41 @@
 
 
 #include "AbilitySystem/SkirmisherAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+USkirmisherAttributeSet::USkirmisherAttributeSet()
+{
+    InitHealth(100.f);
+    InitHealthMax(100.f);
+    InitMana(100.f);
+    InitManaMax(100.f);
+}
+
+void USkirmisherAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME_CONDITION_NOTIFY(USkirmisherAttributeSet, Health, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(USkirmisherAttributeSet, HealthMax, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(USkirmisherAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(USkirmisherAttributeSet, ManaMax, COND_None, REPNOTIFY_Always);
+}
+void USkirmisherAttributeSet::OnRep_Health(const FGameplayAttributeData& PrevHealth) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(USkirmisherAttributeSet, Health, PrevHealth);
+}
+
+void USkirmisherAttributeSet::OnRep_HealthMax(const FGameplayAttributeData &PrevHealthMax) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(USkirmisherAttributeSet, HealthMax, PrevHealthMax);
+}
+
+void USkirmisherAttributeSet::OnRep_Mana(const FGameplayAttributeData &PrevMana) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(USkirmisherAttributeSet, Mana, PrevMana);
+}
+
+void USkirmisherAttributeSet::OnRep_ManaMax(const FGameplayAttributeData & PrevManaMax) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(USkirmisherAttributeSet, ManaMax, PrevManaMax);
+}
--- Source/MysticSkirmisher/Public/AbilitySystem/SkirmisherAttributeSet.h
+++ Source/MysticSkirmisher/Public/AbilitySystem/SkirmisherAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "SkirmisherAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+ 	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +20,31 @@ UCLASS()
 class MYSTICSKIRMISHER_API USkirmisherAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	USkirmisherAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& PrevHealth) const;
+	ATTRIBUTE_ACCESSORS(USkirmisherAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_HealthMax, Category = "Vital Attributes")
+	FGameplayAttributeData HealthMax;
+	UFUNCTION()
+	void OnRep_HealthMax(const FGameplayAttributeData& PrevHealthMax) const;
+	ATTRIBUTE_ACCESSORS(USkirmisherAttributeSet, HealthMax);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& PrevMana) const;
+	ATTRIBUTE_ACCESSORS(USkirmisherAttributeSet, Mana);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ManaMax, Category = "Vital Attributes")
+	FGameplayAttributeData ManaMax;
+	UFUNCTION()
+	void OnRep_ManaMax(const FGameplayAttributeData& PrevManaMax) const;
+	ATTRIBUTE_ACCESSORS(USkirmisherAttributeSet, ManaMax);
 };
