c5a6101df50284463cc380bff4a6dd8b804dc6cb
David-6 <jiseop.63@gmail.com>
2025-02-05 00:15:20 +0900

    Refactoring EffectActor

--- Source/MysticSkirmisher/Private/Actor/SkirmisherEffectActor.cpp
+++ Source/MysticSkirmisher/Private/Actor/SkirmisherEffectActor.cpp
@@ -2,42 +2,33 @@
 
 
 #include "Actor/SkirmisherEffectActor.h"
-#include "Components/SphereComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystemComponent.h"
 #include "AbilitySystem/SkirmisherAttributeSet.h"
+#include "AbilitySystem/SkirmisherAbilitySystemComponent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+
 
 ASkirmisherEffectActor::ASkirmisherEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-	SphereComponent = CreateDefaultSubobject<USphereComponent>("Sphere");
-	SphereComponent->SetupAttachment(GetRootComponent());
 
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
-void ASkirmisherEffectActor::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	// TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const USkirmisherAttributeSet* attributeSet = Cast<USkirmisherAttributeSet>(ASInterface->GetAbilitySystemComponent()->GetAttributeSet(USkirmisherAttributeSet::StaticClass()));
-		USkirmisherAttributeSet* mutableAttributeSet = const_cast<USkirmisherAttributeSet*>(attributeSet);
-		
-		mutableAttributeSet->SetHealth(attributeSet->GetHealth() + 10.f);
-		mutableAttributeSet->SetMana(attributeSet->GetMana() + 10.f);
-		Destroy();
-	}
-}
-void ASkirmisherEffectActor::EndOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex)
-{
-}
+
 void ASkirmisherEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ASkirmisherEffectActor::BeginOverlap);
-	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ASkirmisherEffectActor::EndOverlap);
 }
 
+void ASkirmisherEffectActor::ApplyEffectToTarget(AActor *Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* targetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (targetASC == nullptr) return;
+	check(GameplayEffectClass);
+
+	FGameplayEffectContextHandle effectcontextHandle = targetASC->MakeEffectContext();
+	effectcontextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle effectSpec = targetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, effectcontextHandle);
+	targetASC->ApplyGameplayEffectSpecToSelf(*effectSpec.Data.Get());
+}
--- Source/MysticSkirmisher/Public/Actor/SkirmisherEffectActor.h
+++ Source/MysticSkirmisher/Public/Actor/SkirmisherEffectActor.h
@@ -16,17 +16,13 @@ class MYSTICSKIRMISHER_API ASkirmisherEffectActor : public AActor
 public:	
 	ASkirmisherEffectActor();
 
-	UFUNCTION()
-	virtual void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> SphereComponent;
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 };
