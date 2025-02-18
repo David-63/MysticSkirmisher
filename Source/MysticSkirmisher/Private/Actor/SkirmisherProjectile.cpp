1bb64c35441c4f3feca7713ed6b66bb204ea5a2a
David-6 <jiseop.63@gmail.com>
2025-02-18 16:27:38 +0900

    Add Projectile class

---
+++ Source/MysticSkirmisher/Private/Actor/SkirmisherProjectile.cpp
@@ -0,0 +1,37 @@
+// Copyright David-6
+
+
+#include "Actor/SkirmisherProjectile.h"
+#include "COmponents/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ASkirmisherProjectile::ASkirmisherProjectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	SetRootComponent(Sphere);
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+	ProjectileMovement->InitialSpeed = 450.f;
+	ProjectileMovement->MaxSpeed = 600.f;
+	ProjectileMovement->ProjectileGravityScale = 0.f;
+
+}
+
+void ASkirmisherProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ASkirmisherProjectile::OnSphereBeginOverlap);
+	
+}
+
+void ASkirmisherProjectile::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
---
+++ Source/MysticSkirmisher/Public/Actor/SkirmisherProjectile.h
@@ -0,0 +1,36 @@
+// Copyright David-6
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SkirmisherProjectile.generated.h"
+
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class MYSTICSKIRMISHER_API ASkirmisherProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASkirmisherProjectile();
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+};
